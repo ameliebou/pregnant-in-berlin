f2d73213c90fb2b571ee7d769b03c8690a6f7992
Amelie Boudier <amelie.boudier01@gmail.com>
2019-03-11 18:23:09 +0100

    added validations and associations to models

--- app/models/kita.rb
+++ app/models/kita.rb
@@ -1,2 +1,5 @@
 class Kita < ApplicationRecord
+  has_many :kita_contacts
+
+  validates :name, presence: true
 end
--- app/models/kita_contact.rb
+++ app/models/kita_contact.rb
@@ -1,2 +1,4 @@
 class KitaContact < ApplicationRecord
+  belongs_to :kita
+  belongs_to :user
 end
