d86289505192bdeb0e7aef6e8bb074b4fed27ef0
Amelie Boudier <amelie.boudier01@gmail.com>
2019-04-17 17:26:25 +0200

    added search function

--- app/controllers/kindergartens_controller.rb
+++ app/controllers/kindergartens_controller.rb
@@ -1,6 +1,12 @@
 class KindergartensController < ApplicationController
   def index
-    @kindergartens = Kindergarten.where.not(latitude: nil, longitude: nil)
+    if params[:address].present? && params[:radius].present?
+      @kindergartens = Kindergarten.near(params[:address], params[:radius])
+    elsif params[:address].present?
+      @kindergartens = Kindergarten.near(params[:address], 2)
+    else
+      @kindergartens = Kindergarten.where.not(latitude: nil, longitude: nil)
+    end
     @markers = @kindergartens.map do |kindergarten|
       {
         lng: kindergarten.longitude,
