4646a446ff97537cca3a87eeefee0a17ca1834cc
Amelie Boudier <amelie.boudier01@gmail.com>
2019-06-12 16:13:52 +0200

    added search filter and style

--- app/controllers/kindergartens_controller.rb
+++ app/controllers/kindergartens_controller.rb
@@ -1,15 +1,49 @@
 class KindergartensController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
+  def get_minimum_age
+    case params[:minimum_age]
+    when '< 6'
+      @kindergartens = @kindergartens.where('minimum_age < ?', 6)
+    when '6 - 12'
+      @kindergartens = @kindergartens.where('minimum_age >= ?', 6).where('minimum_age < ?', 12)
+    when '12 - 18'
+      @kindergartens = @kindergartens.where('minimum_age >= ?', 12).where('minimum_age < ?', 18)
+    when '> 18'
+      @kindergartens = @kindergartens.where('minimum_age >= 18', 6)
+    end
+  end
+
+  def get_places
+    case params[:places]
+    when 'all'
+      @kindergartens = @kindergartens
+    when '< 20'
+      @kindergartens = @kindergartens.where('places < ?', 20)
+    when '20 - 50'
+      @kindergartens = @kindergartens.where('places >= ?', 20).where('places < ?', 50)
+    when '50 - 100'
+      @kindergartens = @kindergartens.where('places >= ?', 50).where('places < ?', 100)
+    when '> 100'
+      @kindergartens = @kindergartens.where('places >= 100', 6)
+    end
+  end
+
   def index
     if params[:address].present?
       address = params[:address] + " Berlin"
-      @kindergartens = Kindergarten.near(address, params[:radius])
+      params[:radius].present? ? radius = params[:radius] : radius = 2
+      @kindergartens = Kindergarten.near(address, radius)
+      get_minimum_age
+      get_places
     else
       @kindergartens = Kindergarten.where.not(latitude: nil, longitude: nil)
     end
     @markers = @kindergartens.map do |kindergarten|
       {
+        details: {
+          contacts: kindergarten.kita_contacts.where(user: current_user)
+        },
         lng: kindergarten.longitude,
         lat: kindergarten.latitude,
         infoWindow: render_to_string(partial: "infowindow", locals: { kindergarten: kindergarten })
