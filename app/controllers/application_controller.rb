f19b3bfd6a623ca507404acd7be792d9d0b790de
Amelie Boudier <amelie.boudier01@gmail.com>
2019-06-21 11:25:54 +0200

    fixed devise redirection when signing in or up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
+
   before_action :authenticate_user!
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  private
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
