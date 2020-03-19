f7ff0ccc0e7ef4a07b35ca70bec45d4999b1aafd
Amelie Boudier <amelie.boudier01@gmail.com>
2020-03-19 16:25:11 +0100

    added kita contact list to dashboard

--- app/controllers/kita_contacts_controller.rb
+++ app/controllers/kita_contacts_controller.rb
@@ -2,24 +2,6 @@ class KitaContactsController < ApplicationController
   before_action :set_kita, only: :create
   before_action :set_kita_contact, only: [:destroy, :update]
 
-  def index
-    @kita_contacts = KitaContact.all.where(user: current_user)
-    @kitas = []
-    @kita_contacts.each { |kc| @kitas << kc.kindergarten }
-    @kitas.uniq!
-    @kitas.sort!
-    @markers = @kitas.map do |kita|
-      {
-        details: {
-          contacts: kita.kita_contacts.where(user: current_user)
-        },
-        lng: kita.longitude,
-        lat: kita.latitude,
-        infoWindow: render_to_string(partial: "infowindow", locals: { kindergarten: kita })
-      }
-    end
-  end
-
   def create
     @kita_contact = KitaContact.new(kita_contact_params)
     @kita_contact.kindergarten = @kita
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,21 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!#, only: [:home, :kitas, :about, :midwives]
+
+  def dashboard
+    @kita_contacts = KitaContact.all.where(user: current_user)
+    @kitas = []
+    @kita_contacts.each { |kc| @kitas << kc.kindergarten }
+    @kitas.uniq!
+    @kitas.sort!
+    @markers = @kitas.map do |kita|
+      {
+        details: {
+          contacts: kita.kita_contacts.where(user: current_user)
+        },
+        lng: kita.longitude,
+        lat: kita.latitude,
+        infoWindow: render_to_string(partial: "infowindow", locals: { kindergarten: kita })
+      }
+    end
+  end
 end
