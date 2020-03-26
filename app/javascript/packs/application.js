a356673b0eed21153ed7e8ad45a96194191d350b
Amelie Boudier <amelie.boudier01@gmail.com>
2020-03-26 10:07:53 +0100

    added datepicker class to kita reminder form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,14 @@
 import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'bootstrap';
 
 import { initMapbox } from '../plugins/init_mapbox';
-import { smoothScrolling } from '../plugins/smooth_scrolling'
+import { smoothScrolling } from '../plugins/smooth_scrolling';
 
 document.addEventListener("turbolinks:load", function() {
   initMapbox();
   smoothScrolling();
-})
-
-flatpickr("#datepicker", {
-
 });
+
+flatpickr(".datepicker", {});
